da46b8a2de68152466a5fd7996352ba0137cf211
Alan <mendezalau@gmail.com>
2024-10-21 06:13:29 -0600

    Cambios

--- src/pages/basic/ConditionalPage.tsx
+++ src/pages/basic/ConditionalPage.tsx
@@ -1,6 +1,48 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export const ConditionalPage = () => {
+
+    const { resetCurrentSelection } = useContext(AppContext);
+
     return (
-        <div>ConditionalPage</div>
+        <div className="relative overflow-auto h-full">
+            <button onClick={resetCurrentSelection} className="absolute mt-2 py-1 px-6 rounded-lg bg-red-400 font-semibold">Salir</button>
+            <h1 className="font-semibold text-2xl text-center mt-2">Condicionales</h1>
+            <hr className="mt-2 mb-1" />
+            <p className="text-start px-2 text-sm bg-zinc-500/10 py-1">
+                Las condicionales dentro de javascript nos permite comprobar ciertos valores, datos o variables,
+                existen varios tipos de condicionales, tales como las condicionales simples, multiples o anidadas,
+                estas se deben ocupar solo en ciertas ocasiones para un buen entendimiento del programador
+            </p>
+            <div className="text-start px-2 text-sm bg-zinc-500/10 mt-2 py-1">
+                <p>Hay diversos tipos de condicionales: </p>
+                <p className="mb-1"><strong>Condicion simple:</strong> Son aquellas que comprueban solo si se cumple la condicion </p>
+                <p className="mb-1"><strong>Condicion multiple:</strong> Son aquellas que comprueban si se cumple la condicion y tambien si no se cumple</p>
+                <p className="mb-1"><strong>Switch:</strong> Son aquellas que guardan multiples opciones que resuleven la condicion</p>
+                <p className="mb-1"><strong>Terceario:</strong> Es una alternativa inline de una condicion simple</p>
+            </div>
+            <hr className="mt-2 mb-1" />
+
+            <h1 className="font-semibold text-2xl text-center mt-2">Usos en codigo</h1>
+            <div>
+                <h1 className="font-semibold text-lg mt-1 -mb-1">Condicion simple (if)</h1>
+                <p className="text-start px-2 text-sm bg-zinc-500/10 mt-2 py-1">
+                    Este tipo de condicion te permite ejecutar una seccion del codigo solo si la condicion que esta entre los parentesis
+                    se cumple, de caso contrario no se realizara nada, por ejemplo si quieres comprobar si un nombre tiene tantas letras
+                </p>
+                <img className="mt-2 border border-zinc-300/10" src="images/DataType1.png" width={500} />
+                <hr className="mt-1" />
+            </div>
+            <div>
+                <h1 className="font-semibold text-lg mt-1 -mb-1">Condicion multiple (ifelse | ifelseif)</h1>
+                <p className="text-start px-2 text-sm bg-zinc-500/10 mt-2 py-1">
+                    Este tipo de condicion te permite ejecutar una seccion del codigo solo si la condicion que esta entre los parentesis
+                    se cumple, de caso contrario no se realizara nada, por ejemplo si quieres comprobar si un nombre tiene tantas letras
+                </p>
+                <img className="mt-2 border border-zinc-300/10" src="images/DataType1.png" width={500} />
+                <hr className="mt-1" />
+            </div>
+        </div>
     )
 }
--- src/pages/basic/VariablesPage.tsx
+++ src/pages/basic/VariablesPage.tsx
@@ -1,6 +1,54 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export const VariablesPage = () => {
+    
+    const { resetCurrentSelection } = useContext(AppContext);
+
     return (
-        <div>DataTypePage</div>
+        <div className="relative overflow-auto h-full">
+            <button onClick={resetCurrentSelection} className="absolute mt-2 py-1 px-6 rounded-lg bg-red-400 font-semibold">Salir</button>
+            <h1 className="font-semibold text-2xl text-center mt-2">Variables</h1>
+            <hr className="mt-2 mb-1" />
+            <p className="text-start px-2 text-sm bg-zinc-500/10 py-1">
+                Existen 2 tipos de variables dentro de javascript, las variables de tipo constante
+                y las variables dinamicas o let, las variables constantes son aquellas que no pueden cambiar
+                es decir si tu les asignas que tendran un 1, no podran cambiar a 2 ni a ningun otro tipo de valor,
+                mientras que con las variables dinamicas o let si te permiten cambiar este dato las veces que quieras,
+                es recomendable utilizar constantes dentro de tu codigo ya que te permite un mejor rendimiento
+                y entendimiento dentro de tu codigo!
+            </p>
+            <div className="text-start px-2 text-sm bg-zinc-500/10 mt-2 py-1">
+                <p>Reglas de las variables: </p>
+                <ul>
+                    <li><p className="mb-2"><strong>Variables:</strong> Las variables se declaran con lowerCamelCase, es decir empiezan en minuscula y cada palabra nueva empieza con Mayuscula</p></li>
+                    <li><p className="mb-1"><strong>Funciones:</strong> Las funciones se declaran con upperCamelCase, es decir todas las palabras empiezan con mayuscula</p></li>
+                </ul>
+            </div>
+            <hr className="mt-2 mb-1" />
+            <h1 className="font-semibold text-2xl text-center mt-2">Usos en codigo</h1>
+            <div>
+                <h1 className="font-semibold text-lg mt-1 -mb-1">Constante (Const)</h1>
+                <p className="text-start px-2 text-sm bg-zinc-500/10 mt-2 py-1">
+                    Esta variable como mencione anteriormente el dato que le asignemos sera un dato
+                    que nunca va a cambiar, normalmente se utiliza por ejemplo para PI, tambien
+                    para declaracion de funciones, o para declaracion de objetos, por ejemplo:
+                </p>
+                <img className="mt-2 border border-zinc-300/10" src="images/Variables1.png" width={500} />
+                <hr className="mt-1" />
+            </div>
+            <div>
+                <h1 className="font-semibold text-lg mt-1 -mb-1">Dinamicas (Let)</h1>
+                <p className="text-start px-2 text-sm bg-zinc-500/10 mt-2 py-1">
+                    Esta variable como mencione anteriormente el dato que le asignemos sera un dato
+                    que lo podremos cambiar en todo momento, se puede ocupar en cualquier momento
+                    para funciones u objetos (Aunque no es recomendable), nos da la facilidad de poder
+                    saltarnos las reglas de las constantes permitiendonos cambiar todo, pero con el
+                    inconveniente de que afecta en parte el rendimiento
+                </p>
+                <img className="mt-2 border border-zinc-300/10" src="images/Variables2.png" width={500} />
+                <hr className="mt-1" />
+            </div>
+        </div>
     )
 }
